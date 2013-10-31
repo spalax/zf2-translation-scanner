57f6e31c3f5f23d4a1eb00e7296fe48369b7bc5c
Milotskiy Alexey <spalaxinco@gmail.com>
2013-10-31 09:24:01 +0200

    Change formatting and fix autoload troubles.

--- src/Zf2TranslationScanner/Console.php
+++ src/Zf2TranslationScanner/Console.php
@@ -1,25 +1,26 @@
 <?php
 namespace Zf2TranslationScanner;
 
+use Zend\Console\Getopt;
 use Zf2Libs\Filter\Formatter;
 
-class Console/* extends Zend_Console_Getopt*/  {
-
-	/**
-	 * Internal print function
-	 *
-	 * @param string $template
-	 * @param array $data data for place holders
-	 * @param boolean $new_line
-	 * @param boolean $exit is execution must be stopped after print
-	 * @return void
-	 */
-	public function _print($template = "", $data=array(), $new_line = true, $exit = false)
-	{
-		if ($this->v) {
-            $filter = new Formatter($template.($new_line ? "\n" : ""));
+class Console extends Getopt
+{
+    /**
+     * Internal print function
+     *
+     * @param string $template
+     * @param array $data data for place holders
+     * @param boolean $new_line
+     * @param boolean $exit is execution must be stopped after print
+     * @return void
+     */
+    public function log($template = "", $data = array(), $new_line = true, $exit = false)
+    {
+        if ($this->v) {
+            $filter = new Formatter($template . ($new_line ? "\n" : ""));
             print $filter->filter($data);
-		}
-		if ($exit === true) exit(0);
-	}
+        }
+        if ($exit === true) exit(0);
+    }
 }
